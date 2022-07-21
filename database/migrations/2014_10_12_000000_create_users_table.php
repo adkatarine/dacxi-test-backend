1ce2711aa47364e3853606a68538ec655ddf3ebc
adkatarine <adllakatarine@hotmail.com>
2022-07-21 15:16:03 -0300

    config: adiciona schemas referentes a tabela users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('users');
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
